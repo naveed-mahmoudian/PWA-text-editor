48dd43022caaa5b7be549bc008ffa6bd081b8b0c
Naveed Mahmoudian <nmahmoudian@gmail.com>
2023-01-08 15:00:46 -0800

    Working on install.js scripts

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,33 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  butInstall.style.display = "block";
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  try {
+    butInstall.style.display = "none";
+    deferredPrompt.prompt();
+
+    const choiceResult = await deferredPrompt.userChoice;
+    if (choiceResult.outcome === "accepted") {
+      console.log("PWA setup accepted");
+    } else {
+      console.log("PWA setup rejected");
+    }
+
+    deferredPrompt = null;
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("PWA Installed");
+});
